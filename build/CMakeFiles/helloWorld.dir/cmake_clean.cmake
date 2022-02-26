21710bf511c6c88dd2d94c674e67d8cbd5c7fce0
azeezh <ah88238@uga.edu>
2022-02-26 16:29:35 -0500

    Testing CMake commit

---
+++ build/CMakeFiles/helloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloWorld.dir/src/compiled.cpp.obj"
+  "CMakeFiles/helloWorld.dir/src/compiled.cpp.obj.d"
+  "helloWorld.exe"
+  "helloWorld.exe.manifest"
+  "helloWorld.pdb"
+  "libhelloWorld.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/compiled.cpp
+++ src/compiled.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "practice\tester.h"
+#include "tester.h"
 
 using namespace std;
 using namespace tester;
